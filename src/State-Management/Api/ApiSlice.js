f6ee31c0ae44e76013aecc2b74859cef34117394
kanimerakala Harish Vardhan <kharishvardhan@mylastech.com>
2026-08-07 12:34:42 +0530

    prod: update baseUrl to https://api.benmyl.com for Benmyl-app

--- src/State-Management/Api/ApiSlice.js
+++ src/State-Management/Api/ApiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
  
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://webapidev.benmyl.com",
+  baseUrl: "https://api.benmyl.com",
  
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("token");
